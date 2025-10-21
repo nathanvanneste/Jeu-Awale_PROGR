5cc419380d8165f3f5065a15be5327dc62931609
Vanneste Nathan <nvanneste@if501-219-01.insa-lyon.fr>
2025-10-21 11:48:29 +0200

    Premières structures

--- Serveur/client2.h
+++ Serveur/client2.h
@@ -2,11 +2,32 @@
 #define CLIENT_H
 
 #include "server2.h"
+#include <stdbool.h>
+
+typedef struct
+{
+   char * Joueur1;
+   char * Joueur2;
+   int plateau[12];
+   int cptJoueur1;
+   int cptJoueur2;
+   char * joueurActuel;
+   bool partieEnCours;
+   char ** observers;
+}Partie;
 
 typedef struct
 {
    SOCKET sock;
    char name[BUF_SIZE];
+   Partie ** parties;
 }Client;
+
+typedef struct
+{
+   char * expediteur;
+   char * destinataire;
+   char * contenu;
+}Message;
 
 #endif /* guard */
